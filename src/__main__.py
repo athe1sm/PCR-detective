e2f67d64fad85edc5bf0df16b0886aef625fed59
floodowen <owen.e.flood@gmail.com>
2021-03-15 10:06:49 -0400

    Added setup.py and __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+Command line interface to readfile
+"""
+
+# Potential code below. Will need to be changed
+
+import argparse
+
+def parse_command_line():
+    "parses args for the readfile funtion"
+
+    # init parser and add arguments
+    parser = argparse.ArgumentParser()
+
+    # add long args
+    parser.add_argument(
+        "--clean",
+        help="cleans .fasta file",
+        action="store_true")
+
+
+def main():
+    "run main function on parsed args"
+
+    # get arguments from command line as a dict-like object
+    args = parse_command_line()
+
+    # add if arguments
+
+
+if __name__ == "__main__":
