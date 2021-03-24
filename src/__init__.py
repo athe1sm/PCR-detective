6fe411ed2eb92f9ec2625a9811fd2220d14fd23b
smau8 <sma2244@columbia.edu>
2021-03-24 10:11:26 -0700

    pair programming - add init, update read_file class object, attempt to make CLI installable

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+PCR-detective program __init__ file
+"""
+
+__version__ = "0.0.2"
+
+from src.readfile import read_file
+from src.cleanup import cleanup
--- src/__main__.py
+++ src/__main__.py
@@ -7,6 +7,8 @@ Command line interface to readfile
 # Potential code below. Will need to be changed
 
 import argparse
+# add import statement for readfile.py
+from readfile import read_file
 
 def parse_command_line():
     "parses args for the readfile funtion"
@@ -20,6 +22,10 @@ def parse_command_line():
         help="cleans .fasta file",
         action="store_true")
 
+    # add arg to take in file path location
+
+    args = parser.parse_args()
+
 
 def main():
     "run main function on parsed args"
@@ -27,4 +33,14 @@ def main():
     # get arguments from command line as a dict-like object
     args = parse_command_line()
 
-    # add if arguments
+    # add if arguments here for read_file()
+    if args.clean:
+        seq1 = read_file()
+        print(seq1.readfile())
+        # cleanup(), add when cleanup.py is ready
+
+
+if __name__ == "__main__":
+    seq1 = read_file()
+    print(seq1.readfile())
+
